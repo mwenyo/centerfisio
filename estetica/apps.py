5eb58e9789f678a2974a8a3fa64ef1a7f3b743db
mwenyo <mwenyo@gmail.com>
2019-03-24 19:29:32 -0300

    adição dos apps "estetica", "fisioterapia" e "pilates"

---
+++ estetica/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class EsteticaConfig(AppConfig):
+    name = 'estetica'
---
+++ pilates/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PilatesConfig(AppConfig):
+    name = 'pilates'
